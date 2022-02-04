a9e583bcaa8d83a34907c7a3dad99ae7b306a9de
tonton <mayatonton1994@gmail.com>
2022-02-05 07:47:21 +0900

    update for Cob environment Huawei P30 Pro

--- formation_7x7.py
+++ formation_7x7.py
@@ -1,10 +1,21 @@
 # とりま iphone se でSCした画像から
-SOURCE_IMG_W = 750
-SOURCE_IMG_H = 1623
-OFFSET_X = 30
-OFFSET_Y = 435
-TROOPS_SIZE_W = 95
-TROOPS_SIZE_H = 149
+# iphonese settings
+# SOURCE_IMG_W = 750
+# SOURCE_IMG_H = 1623
+# OFFSET_X = 30
+# OFFSET_Y = 435
+# TROOPS_SIZE_W = 95
+# TROOPS_SIZE_H = 149
+
+# Huawei P30 Pro settings
+SOURCE_IMG_W = 1080
+SOURCE_IMG_H = 2340
+OFFSET_X = 84
+OFFSET_Y = 720
+TROOPS_SIZE_W = 140
+TROOPS_SIZE_H = 198
+
+
 
 formation_array = [ 
     ["🔳", "🔳", "🔳", "🔳", "🔳", "🔳", "🔳"],
@@ -23,7 +34,8 @@ def calc_disploc2charloc(disp_loc_x, disp_loc_y):
     return x, y
 
 def set_char(loc_x, loc_y, troop):
-    formation_array[loc_x][loc_y] = troop
+    if loc_x >= 0 and loc_x <= 6 and loc_y >= 0 and loc_y <= 6:
+        formation_array[loc_x][loc_y] = troop
 
 def get_formation_array():
     return formation_array
